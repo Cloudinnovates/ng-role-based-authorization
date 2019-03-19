3165dde5411558e8c73b69ce98230b5f6ab6f456
Alan Tan Lai Chian <alantan@gic.com.sg>
2019-03-19 10:12:03 +0800

    I'm so sick of boilerplate codes. Created files/folders and coded build of the components

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AuthenticationService } from '@/_services';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.authenticationService.currentUserValue;
+        if (currentUser) {
+            // check if route is restricted by role
+            if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
+                // role not authorised so redirect to home page
+                this.authenticationService.router.navigate(['/']);
+                return false;
+            }
+
+            // authorised so return home
+            return true;
+        }
+
+        // not logged in s oredirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+
+    }
+}
